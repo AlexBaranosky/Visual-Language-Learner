dcce2c5108604132f81fbcb111a30d59a404a8a2
AlexBaranosky <alexander.baranosky@gmail.com>
2010-09-26 00:29:08 -0400

    first commit of my new Rails 3 app, Visual Language Learner

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VisualLanguageLearner::Application.config.secret_token = '28b853619d40c2bac0dd0b82642992cb392e41eafdd04844801c2e9219321e7671b40563753fdd58a002f4decfca40ad35b330d06f8adf4077a873a549b6eebf'
