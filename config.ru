68e6f7fffe4acb8d29e63cc9621dbb7d10eb0e94
AlexBaranosky <alexander.baranosky@gmail.com>
2010-10-06 00:43:00 -0400

    converted to using HAML

--- config.ru
+++ config.ru
@@ -1,4 +1,5 @@
 # This file is used by Rack-based servers to start the application.
 
 require File.dirname(__FILE__) + '/visual_language_learner'
-run VisualLanguageLearner::Application
+
+VisualLanguageLearner.run!
--- visual_language_learner.rb
+++ visual_language_learner.rb
@@ -1,16 +1,18 @@
 require 'rubygems' if RUBY_VERSION < "1.9"
 require 'sinatra/base'
+require 'haml'
 require File.dirname(__FILE__) + '/classes/enumerable_extensions'
 require File.dirname(__FILE__) + '/classes/image_fetcher'
 
 class VisualLanguageLearner < Sinatra::Base
   get '/' do
-    erb :index
+    haml :index
   end
 
   post '/image' do
     words = params[:words].strip.split(/\s+/)
     @image = ImageFetcher.new(*words).random_image
-    erb :image
+    haml :image
   end
 end
+
